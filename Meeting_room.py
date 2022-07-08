49c8b01d333626b097c16ac20f534bafcb62d568
Skyreding <Skyreding@mail.ru>
2022-07-08 21:34:02 +0500

    Обнвил начальное меню, сделал разделение. Логин теперь происходит непосредственно в меню.

--- Meeting_room.py
+++ Meeting_room.py
@@ -6,25 +6,10 @@ import Login as lg
 
 bot = telebot.TeleBot(TOKEN)
 
-#функция вывода списка переговорных.
-def reserveroom(message):
-    global login
-    userid = message.chat.id
-    lg.authentication(message) #авторизация
-    markup = telebot.types.InlineKeyboardMarkup() #инлайн кнопки
-    if lg.result != []:
-        for row in lg.result:
-            city1 = telebot.types.InlineKeyboardButton(text='Уфа', callback_data='Уфа')
-            city2 = telebot.types.InlineKeyboardButton(text='Москва', callback_data='Москва')
-            city3 = telebot.types.InlineKeyboardButton(text='Питер', callback_data='Питер')
-            markup.add(city1, city2, city3)
-            bot.send_message(userid, f'Здравствуйте, {row[0]} {row[1]}, у нас имеются переговорные в 3 городах: Уфа, Питер, Москва. Выберите город', reply_markup=markup)
-
-    else:
-        bot.reply_to(message, 'Доступ запрещен, обратитесь к уполномоченному администратору')
 
 #Ответ на нажатие inline кнопки.
 def callback_button_room(call):
+
         try:
             connection = psycopg2.connect(
                 user='postgres',
@@ -36,6 +21,7 @@ def callback_button_room(call):
 
             # Курсор для выполнения операций с БД
             cursor = connection.cursor()
+
 
             # Выводим все доступные варианты переговорных в данном городе. Визуал будет изменен в дальнейшем.
             #cursor.execute(f'SELECT city,adress,room_number FROM meeting_room WHERE city={call.data}')
@@ -58,5 +44,5 @@ def callback_button_room(call):
                 cursor.close()
                 connection.close()
 
-def reserve_room_id(message):
-    a=1
+def ReserveSelectRoom(message):
+    bot.send_message(f'Хорошо, {message}')
